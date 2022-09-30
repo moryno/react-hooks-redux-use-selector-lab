d37f2521de196319ca5b2c77f8146e8df32d511a
Maurice <mauricenganga41@gmail.com>
2022-09-30 08:54:23 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import usersReducer from "./features/users/usersSlice";
 
-// add imports and code
+const store = createStore(usersReducer);
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
